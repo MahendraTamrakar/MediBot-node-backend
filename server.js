ff3a5ffd9d9705bc5c8a8f7c098ec2f9a0bd644b
Mahendra Kumar Tamrakar <137670852+MahendraTamrakar@users.noreply.github.com>
2025-05-17 18:00:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ app.use(express.json());
 app.use("/chat", chatRoutes);
 
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, '0.0.0.0' () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server listening on port http://localhost:${PORT}`);
 });
